e98d54c2e0938bcacdb125bbb7fbe8547ae389ec
Sebastian Arredondo <jsebastianav8@gmail.com>
2018-11-03 15:14:59 -0600

    Removed assets

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import door from '../public/assets/1_blue_circle.png'
 
 const Index = () => {
-  return <div><img src={door}/></div>;
+  return <div>Hello React!</div>;
 };
 
 ReactDOM.render(<Index />, document.getElementById("index"));
